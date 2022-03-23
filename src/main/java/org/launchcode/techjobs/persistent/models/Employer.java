247348d1f1e27b7978f237ed8389b5e2d240263b
Kaylyn <kaylynvenerable@gmail.com>
2022-03-23 00:10:42 -0500

    updating job.skills

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name = "")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,34 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(max = 500)
+    private String description;
+
+    @ManyToMany(mappedBy ="skills")
+    private List<Job> jobs;
+
+
+    public Skill() {
+
+    }
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
 }
